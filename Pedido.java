7a1dbca97afb131f167db36834abc1df0df5311c
Eduarda Oliveira <145131925+eduardaoliveiragithub@users.noreply.github.com>
2023-10-02 18:39:03 -0300

    Create Pedido.java

---
+++ Pedido.java
@@ -0,0 +1,24 @@
+//Class pedido
+package atividadeSeis;
+
+public class Pedido {
+	
+	private Cliente cliente;
+	private Item[] itens;
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Item[] getItens() {
+		return itens;
+	}
+	public void setItens(Item[] itens) {
+		this.itens = itens;
+	}
+	
+	
+}
