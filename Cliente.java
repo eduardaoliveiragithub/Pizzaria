f205eff6d989712db74c2c43b0252d356a64a741
Eduarda Oliveira <145131925+eduardaoliveiragithub@users.noreply.github.com>
2023-10-02 18:39:59 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,30 @@
+package atividadeSeis;
+
+public class Cliente {
+	private String nome;
+	private String telefone;
+	private String endereço;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereço() {
+		return endereço;
+	}
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+	
+	
+	
+}
