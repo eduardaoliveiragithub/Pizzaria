64df250cd384ac9b7b5090be466ea3647351af87
Eduarda Oliveira <145131925+eduardaoliveiragithub@users.noreply.github.com>
2023-10-02 18:41:00 -0300

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,23 @@
+//Class Item
+package atividadeSeis;
+
+public class Item {
+	private String descricao;
+	private double preço;
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreço() {
+		return preço;
+	}
+	public void setPreço(double preço) {
+		this.preço = preço;
+	}
+	
+	
+}
